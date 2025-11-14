0cb5c5d05e6f71b1a288c0407755b07a4db5d284
AndersDrevland88 <anders.drevland@gmail.com>
2025-11-14 19:03:24 +0100

    Add files via upload

---
+++ obligatorisk 2.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 13 20:38:59 2025
+
+@author: andersdrevland
+"""
+# Arbeidskrav 2 oppgave 1
+
+alder = int(input('Hvilket år er du født? '))
+alder_i_2024 = 2024 - alder
+print(f'Du blir {alder_i_2024} år i løpet av 2024.')
+
+# Oppgave 2
+
+
+
+antall_elever = int(input('Skriv inn antall elever:' ))
+pizza_per_elev = 0.25 * antall_elever
+print(f'Antall pizza blir {pizza_per_elev}')
+import math
+tall = pizza_per_elev
+rundet_opp = math.ceil(tall)
+print(rundet_opp)  # Output blir heltall
+
+# Oppgave 3
+
+  
+import numpy as np
+v_grad = float(input('Skriv inn gradtallet:' ))
+v_rad = v_grad*np.pi/180
+print(f'Omregnet så er gradtallet {v_rad} radianer')
+
+# Oppgave 4
+
+
+Data = {
+        "Norge": {"Hovedstad": "Oslo","innbyggere": 0.634},
+        "England": {"Hovedstad": "London","innbyggere": 8.982},
+        "Frankrike":{"Hovedstad": "Paris","innbyggere": 2.161},
+        "Italia": {"Hovedstad": "Roma","innbyggere": 2.873}
+        } 
+land = input("Hvilket land vil du ha informasjon om hovedstaden:")
+
+if land in Data:
+    Hovedstad = Data[land]["Hovedstad"]
+    innbyggere = Data[land]["innbyggere"]
+    print(f"{Hovedstad} i {land} har {innbyggere} mill. innbyggere." )
+else:    
+    print("Landet finnes ikke i listen")
+
+nytt_land = input("Skriv inn nytt land: ")
+if nytt_land not in Data:
+    hovedstad = input(f"Hva er hovedstaden i {nytt_land}? ")
+    try:
+        innbyggere = float(input(f"Hvor mange millioner innbyggere er det i {hovedstad}? ").replace(",", "."))
+        
+        Data[nytt_land] = {"Hovedstad": Hovedstad, "innbyggere": innbyggere}
+        
+    except ValueError:
+        print("Vennligst skriv inn innbyggertall som tall (eks: 1.5)")
+else:
+    print("Landet finnes allerede.")
+
+print("Oppdatert ")
+for land, info in Data.items():
+    print(f"{land}: Hovedstad: {info['Hovedstad']}, Innbyggere: {info['innbyggere']} mill.")
+
+
+#Oppgave 5
+
+import math
+
+def trekant_halvsirkel(a, b):
+    areal_trekant = (a * b) / 2
+    radius_halvsirkel = b / 2
+    areal_halvsirkel = (math.pi * radius_halvsirkel ** 2) / 2
+    areal_total = areal_trekant + areal_halvsirkel
+
+    buelengde_halvsirkel = math.pi * radius_halvsirkel
+    omkrets = a + b + buelengde_halvsirkel
+
+    return areal_total, omkrets
+
+a = float(input("Skriv inn lengde a: "))
+b = float(input("Skriv inn lengde b: "))
+
+areal, omkrets = trekant_halvsirkel(a, b)
+print(f"Areal: {areal:.2f}, Ytre omkrets: {omkrets:.2f}")
+
+
+#OPppgave 6
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-10, 10, 200)
+y = -x**2 - 5
+
+plt.plot(x, y)
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('f(x) = -x^2 - 5')
+plt.grid(True)
+plt.show()
+
